263fd9aefc85c52257ece0fc4b168175ba1b286c
ford-sennalabs <ford@sennalabs.com>
2019-11-06 09:08:30 +0700

    test fail

--- test/example.test.js
+++ test/example.test.js
@@ -5,6 +5,6 @@ import App from '../src/app.js';
 describe('With Enzyme', () => {
   it('App shows "Hello world"', () => {
     const app = shallow(<App />);
-    expect(app.find('h1').text()).toEqual('Hello');
+    expect(app.find('h1').text()).toEqual('Hello1');
   });
 });
